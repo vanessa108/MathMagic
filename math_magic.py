48f8ad3a3d6eab88ee4a0afa707a6ad9e75d1728
Jarod Lam <jlam96@gmail.com>
2021-10-07 00:03:17 +1000

    Add support for degrees and radians

--- math_magic.py
+++ math_magic.py
@@ -1,13 +1,18 @@
 import streamlit as st
-from math import pi, sin, exp
+from math import pi, sin, exp, radians
 
-def engineer(age):
-    return sin(age*(pi/exp(1)*-9.81/10))
+def engineer(age, unit):
+    if unit == 'Degrees':
+        return sin(radians(age*(pi/exp(1)*9.81/10)))
+    else:
+        return sin(age*(pi/exp(1)*-9.81/10))
 
 def main():
     st.title('Engineering Math Magic')
-    age = st.slider('Select your age', min_value=0, max_value=100)
-    magic = engineer(age)
+    st.latex(r''' \text{engineer age} = \sin\left(\frac{\text{age} \cdot \pi \cdot e \cdot g}{10}\right) ''')
+    unit = st.selectbox('Unit', ['Degrees', 'Radians'])
+    age = st.slider('Select your age', min_value=0, max_value=100, value=21)
+    magic = engineer(age, unit)
     st.metric(label="Your age", value = magic)
 
 if __name__ == "__main__":
