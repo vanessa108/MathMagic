fb4c0bb42ffb5750441cca6d9e14f55abc40ddd5
Vanessa Li <42192290+vanessa108@users.noreply.github.com>
2021-10-07 11:33:02 +1000

    refine calculations

--- math_magic.py
+++ math_magic.py
@@ -12,11 +12,12 @@ st.set_page_config(page_title='Engineering Math Magic')
 def main():
     image = Image.open('./meme.png')
     st.title('Engineering Math Magic')
-    st.latex(r''' \text{engineer age} = \sin\left(\frac{\text{age} \cdot \pi \cdot e \cdot g}{10}\right) ''')
+    st.latex(r''' \text{engineer age} = \sin\left(\frac{\text{age} \cdot \pi  \cdot g}{10 \cdot e}\right) ''')
+    """Please select a unit that best reflects your own circumstances."""
     unit = st.selectbox('Unit', ['Degrees', 'Radians'])
     age = st.slider('Select your age', min_value=0, max_value=100, value=21)
     magic = engineer(age, unit)
-    st.metric(label="Your age", value = magic)
+    st.metric(label="Your age in engineering", value = magic)
     st.image(image)
 
 if __name__ == "__main__":
